26734a934e4c780483d22c417dd23aba28c9a76c
Baptiste veyssiere <baptiste.veyssiere@epitech.eugit config --global user.email baptiste.veyssiere@epitech.eugit config --global user.name veyssi_bgit commit -am 1>
2016-05-31 22:14:19 +0200

    clean

--- src/check_and_add_path.c
+++ src/check_and_add_path.c
@@ -5,7 +5,7 @@
 ** Login   <VEYSSI_B@epitech.net>
 **
 ** Started on  Sat May 28 21:38:00 2016 Baptiste veyssiere
-** Last update Tue May 31 18:05:34 2016 Baptiste veyssiere
+** Last update Tue May 31 22:13:33 2016 Baptiste veyssiere
 */
 
 #include <stdlib.h>
@@ -86,8 +86,6 @@ static int	add_path(char **command, char **env)
       path[my_strlen("/bin")] = 0;
       my_strcpy("/bin", path);
     }
-  /* else if ((path = get_varenv(env, "PATH")) == NULL) */
-  /*   return (1); */
   if ((tab = get_varpath(path)) == NULL)
     return (-1);
   ret = test_path(tab, command);
