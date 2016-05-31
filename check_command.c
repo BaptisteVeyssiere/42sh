fc3440f36b8fc3033a52c20e46e9f44e5631b50c
ilyas semmaoui <ilyas.semmaoui@epitech.eu>
2016-05-31 04:02:53 +0200

    Missing redirection et null command sans la norme epitech

--- check_command.c
+++ check_command.c
@@ -5,12 +5,74 @@
 ** Login   <VEYSSI_B@epitech.net>
 **
 ** Started on  Thu May 26 14:52:40 2016 Baptiste veyssiere
-** Last update Thu May 26 23:39:32 2016 Baptiste veyssiere
+** Last update Tue May 31 04:00:18 2016 ilyas semmaoui
 */
 
 static int	check_command_loop(char *command)
 {
-  return (1);
+  int		i;
+  int		redir;
+  int		count;
+  int		pipe;
+  int		except;
+
+  i = -1;
+  redir = 0;
+  pipe = 0;
+  count = 0;
+  except = 0;
+  if (command[0] == '|')
+    return (1);
+  while (command && command[++i])
+    {
+      if (command[i] == ';')
+	except = 1;
+      if (command[i] == '|' && command[i - 1] != '|' && command[i + 1] != '|')
+	pipe++;
+      if (pipe > 1)
+	return (1);
+      if (redir > 0 &&
+	  (command[i] == '|' || command[i] == '&' || command[i] == ';'))
+	return (2);
+      if (command[i] == '>' || command[i] == '<')
+	{
+	  redir++;
+	  if (redir == 3 ||
+	      (redir == 2 && (command[i - 1] == ' ' || command[i - 1] == '\t')))
+	    return (2);
+	}
+      if (command[i] != '>' && command[i] != '<' && command[i] != ' ' &&
+	  command[i] != '\t')
+	redir = 0;
+      if (pipe > 0 && command[i] == ';')
+	return (1);
+      if (count > 0 && (pipe > 0 || command[i] == ';'))
+	return (1);
+      if ((command[i] == '|' && command[i + 1] == '|') ||
+	  (command[i] == '&' && command[i + 1] == '&'))
+	{
+	  count++;
+	}
+      if (count >= 2 || (count > 0 && pipe > 0))
+	return (1);
+      if (command[i] != '|' && command[i] != '&' && command[i] != ' ' &&
+	  command[i] != '\t')
+	count = 0;
+      if (command[i] != '|' && command[i] != ' ' && command[i] != '\t')
+	pipe = 0;
+      if (command[i] != ';' && command[i] != ' ' && command[i] != '\t' &&
+	  command[i] != '&')
+	except = 0;
+    }
+  if (redir > 0)
+    return (2);
+  if (pipe > 0)
+    return (1);
+  if (except == 1)
+    return (0);
+  if (count > 0)
+    return (1);
+  return (0);
 }
 
 int	check_command(char *command)
