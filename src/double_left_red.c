14f3830bd88b8f821937b7fb67983711bf1a01f6
Baptiste veyssiere <baptiste.veyssiere@epitech.eugit config --global user.email baptiste.veyssiere@epitech.eugit config --global user.name veyssi_bgit commit -am 1>
2016-06-05 17:36:18 +0200

    check

--- src/double_left_red.c
+++ src/double_left_red.c
@@ -5,7 +5,7 @@
 ** Login   <VEYSSI_B@epitech.net>
 **
 ** Started on  Sun Apr 10 00:49:54 2016 Baptiste veyssiere
-** Last update Mon May 30 00:21:14 2016 Baptiste veyssiere
+** Last update Sun Jun  5 17:33:55 2016 Baptiste veyssiere
 */
 
 #include <stdlib.h>
@@ -15,15 +15,16 @@
 #include "mysh.h"
 #include "get_next_line.h"
 
-static void	fill_file(int fd, char *end)
+static int	fill_file(int fd, char *end)
 {
   char		*str;
 
   while ((str = get_next_line(0)) != NULL &&
 	 my_strcmp_strict(str, end) == 0)
     {
-      write(fd, str, my_strlen(str));
-      write(fd, "\n", 1);
+      if (write(fd, str, my_strlen(str)) == -1 ||
+	  write(fd, "\n", 1) == -1)
+	return (-1);
       free(str);
     }
 }
@@ -36,8 +37,8 @@ int	double_left_red(t_interpipe *command)
   mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
   if ((fd = open("/tmp/mysh", O_CREAT | O_TRUNC | O_WRONLY, mode)) == -1)
     return (-1);
-  fill_file(fd, command->input_file);
-  if (close(fd) == -1)
+  if (fill_file(fd, command->input_file) == -1 ||
+      close(fd) == -1)
     return (-1);
   if ((fd = open("/tmp/mysh", O_RDONLY)) == -1)
     return (-1);
